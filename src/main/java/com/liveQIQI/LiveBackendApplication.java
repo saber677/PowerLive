1d5fe5d13097131810d7044c4b86a666b0e6b0ce
luqiqi <luqiqi@cloudbility.com>
2023-01-04 17:50:22 +0800

    init

---
+++ src/main/java/com/liveQIQI/LiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.liveQIQI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liveQIQI/LiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liveQIQI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
